3dad9848777479b7ffea52ae24fb924254dc397c
Roma <tea55565@gmail.com>
2024-11-16 15:24:55 +0300

    new pytests 7

--- calculate.py
+++ calculate.py
@@ -1,5 +1,9 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'calculating')))
+
 import circle
-import square
+import square 
 
 
 figs = ['circle', 'square']
